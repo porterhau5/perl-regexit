9807c7b6ccf88a739f3d214dbafe5d3d6fbe5be6
porterhau5 <porter.thomas.w@gmail.com>
2012-09-14 16:55:28 -0400

    Initial commit
    Initial code functionality. Still lots on the TODO list, but wanted to
    get a version up here before going further.

--- README.md
+++ README.md
@@ -2,3 +2,59 @@ perl-regexit
 ============
 
 A collection of Perl regular expressions
+
+Requirements
+------------
+
+Perl
+
+Usage
+-----
+
+Put the data files in the data/ directory
+
+Update any configuration variables in regexit.conf
+
+perl regexit.pl
+
+By default, the script looks for data files in the data/ directory. Output files are written to the output/ directory. Both of these values can be changed in the regexit.conf file.
+
+By default, the script will run all regular expressions against the data files. These can be turned on/off by editing the regexit.conf file.
+
+The data currently in the data directory is a couple emails from the Enron email data set.
+
+Output
+------
+<pre>
+user@host:~/perl-regexit$ perl regexit.pl
+Current file: data1/commonfile.txt
+Current file: data/set1
+Current file: data/set1/file1.txt
+Current file: data/set1/file2.txt
+Current file: data/set1/file3.txt
+Current file: data/set2
+Current file: data/set2/file4.txt
+Current file: data/set2/file5.txt
+Done!
+Output can be found in: output/
+</pre>
+
+Background
+----------
+
+I often find myself digging around for regular expressions I've written or come across in the past. The idea is to have one Perl script that can match regular expressions against any set of text-based data. It's more of just a regex cookbook than anything. As I write or come across more useful regular expressions, I'll add them here.
+
+Current Expressions
+-------------------
+
+* Credit Cards: Valid major Credit Card numbers. Used for pen testing.
+* Email Addresses: Valid email addresses.
+* Internet URLs: Grab the full URL for mailto, ftp, http, https.
+
+TODO
+----
+* Check for existence of output dir, mkdir if it doesn't exist
+* Add in an optional param to take in single file (or dir)... can default to regexit.conf file if none specified
+* Add in option to print matches to screen
+* Add expression for matching SS numbers
+* Add expression for matching IPv4/IPv6 addreses
---
+++ regexit.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/perl -w
+
+# +---------------------------------------------------------------+
+# | Name: regexit.pl                                              |
+# |                                                               |
+# | Author: Tom Porter                                            |
+# |                                                               |
+# | Purpose: Pull regular expression matches out of file(s)       |
+# |                                                               |
+# | Version: 0.1                                                  |
+# |                                                               |
+# | Date: 2012/09/14                                              |
+# +---------------------------------------------------------------+
+
+use File::Find;
+
+# open regexit.conf and pull out properties
+$conf_file = <regexit.conf>;
+open(CONF_FILE, $conf_file) || die("Could not open file.");
+@conf=<CONF_FILE>;
+close(CONF_FILE);
+
+my ($filedir, $outputdir, $enable_internet_urls, $enable_credit_card, $enable_email_addresses);
+
+foreach $property (@conf)
+{
+	chomp($property);
+	if($property =~ m/data_path=(.*)/i)
+	{
+		$filedir = $1;
+	}
+	if($property =~ m/output_path=(.*)/i)
+	{
+		$outputdir = $1;
+	}
+	if($property =~ m/credit_card=(\d)/i)
+	{
+                $enable_credit_card = $1;
+	}
+	if($property =~ m/email_addresses=(\d)/i)
+	{
+		$enable_email_addresses = $1;
+	}
+        if($property =~ m/internet_urls=(\d)/i)
+        {
+                $enable_internet_urls = $1;
+        }
+}
+
+# open Email addresses file
+$email_addresses_file="$outputdir/email_addresses.out";
+open(EMAIL_ADDRESSES_FILE, ">$email_addresses_file") || die("Could not open $email_addresses_file.");
+
+# open Credit Cards file
+$credit_card_file="$outputdir/credit_cards.out";
+open(CREDIT_CARD_FILE, ">$credit_card_file") || die("Could not open $credit_card_file.");
+
+# open Internet URLs file
+$internet_urls_file="$outputdir/internet_urls.out";
+open(INTERNET_URLS_FILE, ">$internet_urls_file") || die("Could not open $internet_urls_file.");
+
+# main subroutine for iterating through each file and matching records against the regular expressions
+sub eachFile 
+{
+	my $filename = $_;
+	my $fullpath = $File::Find::name;
+
+	# skip "." and ".."
+	if($filename !~ m/^\.\.?$/i)
+	{
+		print "Current file: $fullpath\n";
+		# open file, write records to array, close file
+		open(WORKING_FILE, $filename) || die("Could not open $filename.");
+		@raw_data=<WORKING_FILE>;
+		close(WORKING_FILE);
+
+		foreach $record (@raw_data)
+		{
+			# Remove \n from end of record
+			chomp($record);
+			$message = $record;
+
+			# Internet URLs section begins
+			if($enable_internet_urls)
+			{
+				while($message =~ m/(((mailto\:|(news|(ht|f)tp(s?))\:\/\/){1}\S+))/gi)
+				{	
+					print INTERNET_URLS_FILE "$1\n";
+				}
+			}
+
+			# Credit card section begins
+			# along with whitespace and -, add .
+			if($enable_credit_card)
+			{
+				while($message =~ m/((\b4[0-9]{12}(?:[0-9]{3})?\b)|(\b(?:\d[ -]*?){13,16}\b))/gi)
+				{
+					print CREDIT_CARD_FILE "$1\n";
+				}
+			}
+
+			# email addresses section begins
+			if($enable_email_addresses)
+			{
+				while($message =~ m/(\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b)/gi)
+				{
+					print EMAIL_ADDRESSES_FILE "$1\n";
+				}
+			}
+		}
+	}
+}
+
+find (\&eachFile, $filedir);
+
+print "Done!\n";
+print "Output can be found in: $outputdir/\n";
+
+# close output files
+close(INTERNET_URLS_FILE);
+close(CREDIT_CARD_FILE);
+close(EMAIL_ADDRESSES_FILE);
